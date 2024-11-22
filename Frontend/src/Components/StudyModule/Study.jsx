4de411fb1b46bd35c2e3eeeb90f5ef1a07dae804
Arghya Bhattacharjee <arghyabhattacharjee876@gmail.com>
2024-11-22 21:07:11 +0530

    Study Module Added

---
+++ Frontend/src/Components/StudyModule/Study.jsx
@@ -0,0 +1,143 @@
+import React from 'react'
+import logo from "../../assets/images/logo.png";
+import background from "../../assets/images/background.png";
+import StudyEle from './StudyEle';
+
+const Study = () => {
+  return (
+    <>
+    <div className="cardsBody">
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+        <StudyEle
+            image={logo}
+            titel="Web Devlopment"
+        />
+        <StudyEle
+            image={background}
+            titel="Java Programming"
+        />
+    </div>    
+    </>
+  )
+}
+
+export default Study;
---
+++ Frontend/src/Components/StudyModule/StudyEle.jsx
@@ -0,0 +1,14 @@
+import "./Study.css";
+
+const StudyEle = (props) =>{
+    return (
+        <div className="cards">
+            <img className="cardsPic" src={props.image} alt="img" />
+            <div className="cardsTitel">
+                <p>{props.titel}</p>
+            </div>
+        </div>
+    );
+}
+
+export default StudyEle;
