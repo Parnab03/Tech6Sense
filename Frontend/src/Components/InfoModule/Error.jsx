aeee905b3284f512d9ccaeb6c191982580601fcb
Parnab Bagchi <pb7439578071@gmail.com>
2024-11-30 00:00:51 +0530

    Update Error.jsx

--- Frontend/src/Components/InfoModule/Error.jsx
+++ Frontend/src/Components/InfoModule/Error.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import NotFound from "/404NotFound.svg";
+import NotFound from "./404NotFound.svg"; // Ensure correct path
 import styles from "./Error.module.css";
 import { NavLink } from "react-router-dom";
-const Error = () => {
-    return (
-        <div className={styles.container}>
-            <img className={styles.error} src={NotFound} alt="404 Error" />
-            <button className={styles.button}><NavLink className={styles.link} to="/">Go Home</NavLink></button>
-        </div>
 import Navbar from "../CommonModule/Navbar/Navbar";
 import Footer from "../CommonModule/Footer/Footer";
+
 const Error = () => {
     return (
         <>
             <Navbar />
             <div className={styles.container}>
                 <img className={styles.error} src={NotFound} alt="404 Error" />
+                <button className={styles.button}>
+                    <NavLink className={styles.link} to="/">Go Home</NavLink>
+                </button>
             </div>
             <Footer />
         </>
